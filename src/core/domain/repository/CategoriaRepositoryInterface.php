4d80c7e65f10520dd9961dac278dd930c29864ee
Edson Koguishi <edson.koguishi@amaiseducacao.com.br>
2024-08-07 06:14:28 -0300

    feat: interface paginacao

--- src/core/domain/repository/CategoriaRepositoryInterface.php
+++ src/core/domain/repository/CategoriaRepositoryInterface.php
@@ -12,4 +12,10 @@ interface CategoriaRepositoryInterface
     public function delete(string $id): void;
 
     public function readAll(string $filter = '', string $order = 'DESC'): array;
+    public function paginate(
+        string $filter = '',
+        string $order = 'DESC',
+        int $page = 1,
+        int $totalPage = 15
+    ): PaginationInterface;
 }
